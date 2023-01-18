42f54a00f1b0f422e955eaf9d2c029726f01da8c
¡Duque <atanaelleonardo@gmail.com>
2023-01-17 21:54:14 -0300

    chore: using zod

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-interface User {
-  name: string;
-  age: number;
-}
+import z from "zod";
+
+const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Thou name needs at least 3 characters" })
+    .transform((name) => name.toUpperCase()),
+  age: z.number().min(18, { message: "Thou need to be of legal age." }),
+});
+type User = z.infer<typeof userSchema>;
 
 function saveUserToDatabase(user: User) {
-  //
+  const { name, age } = userSchema.parse(user);
 
-  console.log(user);
+  console.log(name, age);
 }
 
 saveUserToDatabase({
